2bcce8e7bba3ba488f1a0b4acc9f701bbfc86d45
Deep <deepdas1816@gmail.com>
2022-02-02 21:58:09 +0530

    initial commit

---
+++ src/context.js
@@ -0,0 +1,125 @@
+import axios from "axios";
+import React, { useState, useContext, useEffect } from "react";
+
+const table = {
+  sports: 21,
+  history: 23,
+  politics: 24,
+  books: 10,
+  GK: 9,
+  film: 11,
+  science_nature: 17,
+  computers: 18,
+  geography: 22,
+  maths: 19,
+  art: 25,
+  animals: 27,
+};
+
+const API_ENDPOINT = "https://opentdb.com/api.php?";
+
+const url = "";
+const tempUrl =
+  "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [waiting, setWaiting] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  const [error, setError] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [quiz, setQuiz] = useState({
+    amount: 10,
+    category: "sports",
+    difficulty: "easy",
+  });
+
+  const fetchQuestions = async (url) => {
+    setLoading(true);
+    setWaiting(false);
+    const response = await axios(url).catch((err) => console.log(err)); //performs get request by default
+    if (response) {
+      const data = response.data.results;
+      if (data.length > 0) {
+        setQuestions(data);
+        setLoading(false);
+        setWaiting(false);
+        setError(false);
+      } else {
+        setWaiting(true);
+        setError(true);
+      }
+    } else {
+      setWaiting(true);
+    }
+  };
+
+  const nextIndex = () => {
+    setIndex((oldIndex) => {
+      const index = oldIndex + 1;
+      if (index > questions.length - 1) {
+        openModal();
+        return 0;
+      } else {
+        return index;
+      }
+    });
+  };
+  const checkAnswer = (value) => {
+    if (value) {
+      setCorrect(correct + 1);
+    }
+    nextIndex();
+  };
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setWaiting(true);
+    setCorrect(0);
+    setIsModalOpen(false);
+  };
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setQuiz({ ...quiz, [name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { amount, category, difficulty } = quiz;
+    const url = `${API_ENDPOINT}amount=${amount}&difficulty=${difficulty}&category=${table[category]}&type=multiple`;
+    fetchQuestions(url);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        waiting,
+        loading,
+        questions,
+        index,
+        correct,
+        error,
+        isModalOpen,
+        nextIndex,
+        checkAnswer,
+        closeModal,
+        quiz,
+        handleChange,
+        handleSubmit,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
